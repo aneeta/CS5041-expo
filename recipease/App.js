8e03b5318261f1e3236004eb27251714cefc789c
Aneta <anette812s@gmail.com>
2023-03-05 21:32:13 +0000

    cleanup

--- recipease/App.js
+++ recipease/App.js
@@ -1,79 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Layout from './src/pages/layout';
-// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Component } from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 
-
-import BaseLayout from './src/components/base/Layout';
 import HomePage from './src/pages/home';
-// import { Router } from 'react-router-dom';
-// import { Switch } from 'react-native-paper';
-import { Component } from 'react';
 import RecipePage from './src/pages/recipe';
 import ProfilePage from './src/pages/profile';
+import AddRecipePage from './src/pages/addRecipe';
+import AddIngPage from './src/pages/addIngredient';
+import IngPage from './src/pages/ingredients';
 
-// export default function App() {
-//   return (
-//     <Router>
-//         {/* <HomePage/> */}
-//         <Switch>
-//           <Route exact path="/" component={HomePage}/>
-//         </Switch>
-//     </Router>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <HomePage/>
-//     // <Router>
-//     //   <Switch>
-//     //     <Route exact path="/" component={HomePage}/>
-//     //   </Switch>
-//     // </Router>
-    
-//     // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Layout>Hello!</Layout>} />
-    //   </Routes>
-    // </BrowserRouter>
-
-//   );
-// }
 
 class App extends Component {
   render() {
     return (
-      // <HomePage/>
-      // <Router>
-      //   {/* <HomePage/> */}
-      //   <Switch>
-      //     <Route exact path="/" component={HomePage}/>
-      //   </Switch>
-      // </Router>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={HomePage()} />
-        <Route path='/profile' element={ProfilePage()} />
-        <Route path='/add-recipe' element={RecipePage()} />
-        <Route path='/browse-recipes' element={ProfilePage()} />
-        <Route path='/add-ingredient' element={RecipePage()} />
-        <Route path='/browse-ingredients' element={RecipePage()} />
-      </Routes>
-    </BrowserRouter>
-      
+        <Routes>
+          <Route path='/' element={HomePage()} />
+          <Route path='/profile' element={ProfilePage()} />
+          <Route path='/add-recipe' element={AddRecipePage()} />
+          <Route path='/browse-recipes' element={RecipePage()} />
+          <Route path='/add-ingredient' element={AddIngPage()} />
+          <Route path='/browse-ingredients' element={IngPage()} />
+        </Routes>
+      </BrowserRouter>
+
     )
   }
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
