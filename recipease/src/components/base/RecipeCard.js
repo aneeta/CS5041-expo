b4043ebe346d4e99c8d46a4e05efb039491ffb93
jl384 <jl384@pc7-107-l.cs.st-andrews.ac.uk>
2023-03-05 20:43:43 +0000

    add

--- recipease/src/components/base/RecipeCard.js
+++ recipease/src/components/base/RecipeCard.js
@@ -6,16 +6,26 @@ class RecipeCard extends Component{
     render(){
         return(
             <div className='card-container'>
-                <div className='card-img'>
-                    
-                </div>
-                <div className='card-user'>
+                <div className='card-inner-box'>
+                    <div className='card-img-box'>
+                        <img className='card-img' src='https://cdn2.thecatapi.com/images/6u9.jpg'></img>
+                    </div>
+                    <div className='card-user' >
+                        <div className='card-avatar-box'>
+                            <img className='card-avatar' src='https://cdn2.thecatapi.com/images/6u9.jpg' ></img>
+                        </div>
+                        <div className='card-avatar-box'>
+                            <span className='card-username'>UserName</span>
+                        </div>
+                    </div>
+                    <div className='card-text-box'>
+                        <div className='card-text'>
+                            <span>spaceholder spaceholder spaceholder spaceholder</span>
+                        </div>
 
+                    </div>
                 </div>
-                <div className='card-text'>
 
-
-                </div>
             </div>
         );
     }
