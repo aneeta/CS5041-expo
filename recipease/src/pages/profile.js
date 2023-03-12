61bcbc72b9d410003de31af3e0efb4b7cb1e2bf1
jl384 <jl384@pc7-037-l.cs.st-andrews.ac.uk>
2023-03-12 15:55:34 +0000

    add

--- recipease/src/pages/profile.js
+++ recipease/src/pages/profile.js
@@ -2,6 +2,14 @@ import BaseLayout from "../components/base/Layout";
 import UserHeader from "../components/base/UserHeader";
 import CardBox from "../components/base/CardBox";
 import  './profile.css';
+import { FloatButton } from 'antd';
+import {
+    PlusOutlined
+  } from '@ant-design/icons';
+
+//check the APIs (like onclick) about floatbutton on ant design website.
+const FBtn = () => <FloatButton icon={<PlusOutlined />} tooltip={<div>Add a recipe</div>} />;
+
 const ProfilePage = (props) => (
     <BaseLayout>
         <div className="profile-container">
@@ -10,6 +18,9 @@ const ProfilePage = (props) => (
             </div>
             <div className="user-content">
             <CardBox></CardBox>
+            </div>
+            <div >
+                <FBtn></FBtn>
             </div>
         </div>
     </BaseLayout>
